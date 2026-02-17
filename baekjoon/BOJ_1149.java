8f41c2e706d335097f2e3a1cc5c921d56136024c
se0hyun <be8shyn@gmail.com>
2026-02-17 15:40:47 +0900

    BOJ 1149 RGB 거리

--- baekjoon/BOJ_1149.java
+++ baekjoon/BOJ_1149.java
@@ -1,16 +1,18 @@
 // BOJ 1149 RGB 거리
-package baekjoon.java;
 
 import java.util.*;
 import java.io.*;
 public class BOJ_1149 {
+    private static int N;
+    private static int[][] dp;
+    private static int[][] costs;
     
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         
-        int N = Integer.parseInt(br.readLine());
-        int[][] dp = new int[N][3];
-        int[][] costs = new int[N][3];
+        N = Integer.parseInt(br.readLine());
+        dp = new int[N][3];
+        costs = new int[N][3];
 
         for(int i = 0; i < N; i++){
             StringTokenizer st = new StringTokenizer(br.readLine());
@@ -30,17 +32,8 @@ public class BOJ_1149 {
                 dp[i][1] = Math.min(dp[i-1][0], dp[i-1][2]) + costs[i][1];
                 dp[i][2] = Math.min(dp[i-1][0], dp[i-1][1]) + costs[i][2];
             }
-            // else if(i == N-1){
 
-            // }
-            // if(i > 0) System.out.println("i - 1: " + (i - 1) + " " + dp[i - 1][0] + " " + dp[i - 1][1] + " " + dp[i - 1][2]);
-            // System.out.println("i: " + i + " " + dp[i][0] + " " + dp[i][1] + " " + dp[i][2]);
-            // System.out.println();
         }
         System.out.println(Math.min(dp[N-1][0], Math.min(dp[N-1][1], dp[N-1][2])));
     }
 }
-
-// 26 40 83
-// 49 60 57  89 75 75
-// 13 89 99  88 88 88
